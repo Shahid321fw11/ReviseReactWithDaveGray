6042c775b65afc869ea200d14ad9823549c6113a
Shahid321fw11 <sidansari20@gmail.com>
2023-01-24 18:33:16 +0530

    changes

--- challenge_two_fetch/src/App.js
+++ challenge_two_fetch/src/App.js
@@ -1,27 +1,8 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import Form from './Form';
 
 function App() {
-  const API_URL = 'https://jsonplaceholder.typicode.com/';
-  const [reqType,setReqType] = useState('users');
-  const [item,setItem] = useState([]);
-
-  useEffect(()=>{
-    const fetchItems = async()=>{
-      try {
-        const response  = await fetch(`${API_URL}${reqType}`);
-        const data = await response.json();
-        setItem(item);
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    fetchItems();
-  },[reqType]);
-
   return <>
-    <Form reqType={reqType} setReqType={setReqType}/>
+  <h1>hello world</h1>
 
   </>
 }
